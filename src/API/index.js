77882d8bd4da3a8ffa47ad74887a0251e46f6d92
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2019-01-30 16:05:21 -0600

    feat:Foursquare API incorporated to be used successfully in the code

---
+++ src/API/index.js
@@ -0,0 +1,50 @@
+class Helper {
+    static baseURL() {
+        return "https://api.foursquare.com/v2";
+    }
+    static auth() {
+        const keys = {
+            client_id:"LFNSSANNNFBBRFQO0MOQYD5PCY23WARBFYM53HXQESRWNUVJ",
+            client_secret:"NIZBI3VGHJWZXKUW4CYI2XOWQRSYEZCJL1WGODLN3DJK5UHU",
+            v:'20190130'
+        }
+        return Object.keys(keys)
+            .map(key => `${key}=${keys[key]}`)
+            .join("&");
+    }
+    static urlBuilder(urlParams){
+        if(!urlParams){
+            return '';
+        }
+        return Object.keys(urlParams)
+            .map(key => `${key}=${urlParams[key]}`)
+            .join("&");
+    }
+    static headers() {
+        return {
+            Accept:"application/json"
+        };
+    }
+    static simpleFetch(endPoint,method,urlParams) {
+        let requestData = {
+            method,
+            headers: Helper.headers()
+        };
+        return fetch(
+            `${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(urlParams)}`,
+            requestData)
+            .then(result => result.json());
+    }
+    
+}
+export default class FoursquareAPI {
+    static search(urlParams){
+        return Helper.simpleFetch("/venues/search","GET",urlParams);
+    }
+    static getVenueDetails(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`,"GET");
+    }
+    static getVenuePhotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`,"GET");
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './components/Map'
-
+import React, { Component } from "react";
+import "./App.css";
+import Map from "./components/Map";
+import FoursquareAPI from "./API/"
 
 class App extends Component {
+  componentDidMount(){
+    FoursquareAPI.search({
+      near:"Des Plaines,IL",
+      query:"pho",
+      limit:10
+    }).then(results => console.log(results));
+  }
+
   render() {
     return (
       <div className="App">
