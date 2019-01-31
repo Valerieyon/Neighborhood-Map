c97aaa2483db150501db3f6fa907e799e8dafd3d
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2019-01-31 15:45:42 -0600

    feat:Markers for specific locations added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,25 +1,31 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+//import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer';
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={8}
+    zoom={props.zoom}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    center={props.center}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.markers &&
+      props.markers.filter(marker => marker.isVisible).map((marker, idx) =>
+        (<Marker key={idx} position={{ lat: marker.lat, lng: marker.lng }} />
+        ))}
   </GoogleMap>
 ))
 
- export default class Map extends Component {
-     render() {
-         return(
-            <MyMapComponent
-            isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBJAQphhfpxArJ40EuKiTXb9qGytot_Flg"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-         )
-     }
- }
+export default class Map extends Component {
+  render() {
+    return (
+      <MyMapComponent
+        {...this.props}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBJAQphhfpxArJ40EuKiTXb9qGytot_Flg"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    )
+  }
+}
